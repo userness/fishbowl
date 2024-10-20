acab99277d11cceed3c8db424638de2b38e59065
auth.microsoft.no.reply <auth.microsoft.no.reply@gmail.com>
2024-10-20 15:31:54 +0000

    Update

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -4,7 +4,7 @@
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
-  packages = [ pkgs.python3 ];
+  packages = [ pkgs.python3 pkgs.sudo];
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [ "ms-python.python" "rangav.vscode-thunder-client" ];
