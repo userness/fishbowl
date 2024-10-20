b187c435096ce67e4011f95d788e614767d30be2
userness <auth.microsoft.no.reply@gmail.com>
2024-10-20 16:50:26 +0000

    ChatGPT'd devserver.sh

--- devserver.sh
+++ devserver.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-source .venv/bin/activate
-python -m flask --app main run --debug
+. .venv/bin/activate
+#python -m flask --app main run --debug
+export FLASK_APP=main.py
+export FLASK_ENV=development
+flask run --host=0.0.0.0 --port=${PORT:-5000}
